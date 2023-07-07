2752e84d3595318b885dbdfa8060f459233a2de3
roninzandy <suddenlyandy@gmail.com>
2023-07-07 14:03:11 +0600

    добавил вывод на сайт меню + ссылки
    css учтено

--- flsite.py
+++ flsite.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
-
+message = 'Наполнение сайта'
+menu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "contact"}]
 @app.route('/')
 def index():
-
-    return render_template('index.html', title='Сайт')
-
+    return render_template('index.html', title='Сайт', menu=menu, message=message)
+# @app.route('/contact')
+# def contact():
+#     return render_template('contact.html', title='Контакты')
 # @app.route('/username/<path>')
 # def username(path):
 #
