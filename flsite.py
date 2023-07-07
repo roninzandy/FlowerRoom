94e7fc2d1177ac9cc10ceee00a39824a147bc443
roninzandy <suddenlyandy@gmail.com>
2023-07-07 23:01:20 +0600

    errorhandler, функции redirect и abort

--- flsite.py
+++ flsite.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'rnnz12345'
@@ -24,6 +24,23 @@ def contact():
 @app.errorhandler(404)
 def pageNotFound(error):
     return render_template('page404.html', title='Страница не найдена', menu=menu), 404 #писать 404 необязательно
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if 'username' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == 'selfedu' and request.form['psw'] == '123':
+        session['userLogged'] = request.form['username']
+        print(session)
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title='Авторизация', menu=menu)
+
+@app.route('/profile/<username>')
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+    return f'Профиль пользователя: {username}'
 
 if __name__ == '__main__':
     app.run(debug=True)
