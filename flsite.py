13991f4dfe8e5639384880b0827329e97c060bda
roninzandy <suddenlyandy@gmail.com>
2023-07-08 23:21:29 +0600

    подключение БД

--- flsite.py
+++ flsite.py
@@ -1,14 +1,69 @@
-from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
+import os
+import sqlite3
+
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort, g
+
+from FDataBase import FDataBase
+
+#конфигурация
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+SECRET_KEY = 'rnnz12345'
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'rnnz12345'
-message = 'Наполнение сайта'
-menu = [{"name": "Установка", "url": "install-flask"},
-        {"name": "Первое приложение", "url": "first-app"},
-        {"name": "Обратная связь", "url": "contact"}]
-@app.route('/')
+app.config.from_object(__name__) #загружаем конфигурацию из приложения (from_object)
+
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db'))) #переопределяем путь к БД
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def create_db():
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        cursor = db.cursor()
+        cursor.executescript(f.read())
+        results = cursor.fetchall()  # Не имеет смысла для операторов типа CREATE, INSERT, UPDATE, DELETE
+    db.commit() #сохрание БД
+    db.close() #закрытие БД
+
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+@app.route("/")
 def index():
-    return render_template('index.html', title='Сайт', menu=menu, message=message)
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('index.html', menu = dbase.getMenu())
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
+@app.route("/add_post", methods=["POST", "GET"])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'])
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавлена успешно', category='success')
+        else:
+            flash('Ошибка добавления статьи', category='error')
+
+    return render_template('add_post.html', menu=dbase.getMenu(), title="Добавление статьи")
+
+
 
 @app.route('/contact', methods=["POST","GET"])
 def contact():
@@ -31,7 +86,6 @@ def login():
         return redirect(url_for('profile', username=session['userLogged']))
     elif request.method == 'POST' and request.form['username'] == 'selfedu' and request.form['psw'] == '123':
         session['userLogged'] = request.form['username']
-        print(session)
         return redirect(url_for('profile', username=session['userLogged']))
 
     return render_template('login.html', title='Авторизация', menu=menu)
@@ -42,8 +96,9 @@ def profile(username):
         abort(401)
     return f'Профиль пользователя: {username}'
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
 # with app.test_request_context():
 #     print(url_for('index'))
+
+if __name__ == "__main__":
+    app.run()
+
